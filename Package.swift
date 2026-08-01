b98f9b298d987163cf7a6e2c5764a5387c865f9f
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-08-01 00:02:42 +0000

    IMGLYPluginBackgroundRemoval version 1.80.0-rc.0

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/imgly/IMGLYUI-swift.git", exact: "1.79.0"),
+    .package(url: "https://github.com/imgly/IMGLYUI-swift.git", exact: "1.80.0-rc.0"),
   ],
   targets: [
     .binaryTarget(
